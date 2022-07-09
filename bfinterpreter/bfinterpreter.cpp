041f93cc65f12fee1191ce4e8827e299a87a1af7
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-07-09 19:25:14 +0900

    command line parsing

---
+++ bfinterpreter/bfinterpreter.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+int main(int argc, char** argv) {
+	void* var = 0;
+	auto memsize =(size_t) 1 << 15;
+	bool warning = true;
+	for (
+		int t = 0;
+		t < argc;
+		t++
+		) {
+	}
+	return 0;
+}
